b93dc00ac6b4b9abb75519a60ba7fdf0db1127d8
Tom Xue <tomxue0126@gmail.com>
2012-11-12 17:27:57 +0800

    use MyEclipse

--- GenDemo.java
+++ GenDemo.java
@@ -1,22 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* the original code is from link: http://edu.qudong.com/safe/base/Javascript/shilidaima/20080514/12527.html */
-
-package gendemo;
-
-/**
- *
- * @author tomxue
- */
+package Java_GP;
 
 public class GenDemo {
 
 	public static class Gen<T> {
 
-		private T ob; //定义泛型成员变量
+		private T ob; // 定义泛型成员变量
 
 		public Gen(T ob) {
 			this.ob = ob;
@@ -36,18 +24,18 @@ public class GenDemo {
 	}
 
 	public static void main(String[] args) {
-		//定义泛型类Gen的一个Integer版本
+		// 定义泛型类Gen的一个Integer版本
 		Gen<Integer> intOb = new Gen<Integer>(88);
 		intOb.showType();
 		int i = intOb.getOb();
-		System.out.println("value= " + i);
+		System.out.println("value = " + i);
 
 		System.out.println("----------------------------------");
 
-		//定义泛型类Gen的一个String版本
+		// 定义泛型类Gen的一个String版本
 		Gen<String> strOb = new Gen<String>("Hello Gen!");
 		strOb.showType();
 		String s = strOb.getOb();
-		System.out.println("value= " + s);
+		System.out.println("value = " + s);
 	}
 }
